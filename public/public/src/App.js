4a6cab5fde73c6135d100a003ec3e256ab2893a1
DevJefersonAbreu <devjefersonabreu10@gmail.com>
2024-08-24 12:46:27 -0300

    add e corrigido filtros-ordenacao-e-busca-exercicios

---
+++ public/public/src/App.js
@@ -0,0 +1,88 @@
+import { useState } from "react";
+import styled, { createGlobalStyle } from "styled-components";
+import pokemons from "./pokemon/pokemon.json";
+import PokemonCard from "./components/PokemonCard/PokemonCard";
+import { getColors } from "./utils/ReturnCardColor";
+import Header from "./components/Header/Header.js";
+const GlobalStyle = createGlobalStyle`
+  *{
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+    font-family: "Inter", sans-serif;
+  
+  }
+`;
+const CardsContainer = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(440px, 1fr));
+  justify-items: center;
+`;
+function App() {
+  const [pesquisa, setPesquisa] = useState("");
+  const [idFilter, setIdFilter] = useState("");
+
+  return (
+    <>
+      <GlobalStyle />
+      <Header
+        idFilter={idFilter}
+        setIdFilter={setIdFilter}
+        pesquisa={pesquisa}
+        setPesquisa={setPesquisa}
+      />
+      <CardsContainer>
+        {pokemons.filter((pokemon) => {
+          return idFilter ? pokemon.id.includes(idFilter) : pokemon
+        })
+          .filter((pokemon) => {
+            return pokemon.name.english.toLowerCase().includes(pesquisa.toLowerCase());
+          })
+          .map((pokemon) => {
+            return (
+              <PokemonCard
+                cardColor={getColors(pokemon.type[0])}
+                key={pokemon.id}
+                pokemon={pokemon}
+              />
+            );
+          })}
+      </CardsContainer>
+    </>
+  );
+}
+
+export default App;
+
+
+const [typeFilter, setTypeFilter] = useState("");
+
+const filteredPokemons = pokemons
+  .filter((pokemon) => {
+    return idFilter ? pokemon.id.includes(idFilter) : pokemon;
+  })
+  .filter((pokemon) => {
+    return pokemon.name.english.toLowerCase().includes(pesquisa.toLowerCase());
+  })
+  .filter((pokemon) => {
+    return typeFilter ? pokemon.type.includes(typeFilter) : pokemon;
+  });
+
+
+
+const handleTypeFilter = (e) => {
+  props.setTypeFilter(e.target.value);
+};
+
+
+const [order, setOrder] = useState("");
+
+const sortedPokemons = [...filteredPokemons].sort((a, b) => {
+  if (order === "asc") {
+    return a.name.english.localeCompare(b.name.english);
+  } else if (order === "desc") {
+    return b.name.english.localeCompare(a.name.english);
+  } else {
+    return 0;
+  }
+});
